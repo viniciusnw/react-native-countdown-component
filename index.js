c2f34846dadc9d876b9cfe5b53b54081bc27e3df
Vinicius <vminacio@paschoalotto.com.br>
2021-05-24 00:29:11 -0300

    edit renderDigit

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ class CountDown extends React.Component {
     return (
         <View style={[
           styles.digitCont,
-          {width: size * 2.3, height: size * 2.6},
+          // {width: size * 2.3, height: size * 2.6},
           digitStyle,
         ]}>
           <Text style={[
